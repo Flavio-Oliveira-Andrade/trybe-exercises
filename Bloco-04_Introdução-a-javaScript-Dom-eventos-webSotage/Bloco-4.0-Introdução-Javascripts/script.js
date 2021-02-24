3981a713e6225c2e168c439143f2d94ebbd6a9fc
Flavio Andrade <foa12381@gmail.com>
2021-02-24 15:50:11 -0300

    exercios de js

---
+++ Bloco-04_Introdução-a-javaScript-Dom-eventos-webSotage/Bloco-4.0-Introdução-Javascripts/script.js
@@ -0,0 +1,43 @@
+/* let movie ="Avenger" string literal
+   let score = 10.89 ; number literal 
+   let isValid = false ; boolean literal 
+   let name; undefined
+   let color = null; redefinition
+
+   + - * / ** %
+   ++
+   --
+  
+ */
+
+
+let movie = "Avenger"; 
+const nome = "Flavio";
+const birthCity = "Rio de Janeiro";
+let birthYear = 1982;
+birthYear =  2020;
+
+
+console.log(nome);
+console.log(birthCity);
+console.log (birthYear);
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = base*2 + altura*2;
+
+
+
+console.log(area);
+console.log(perimetro);
