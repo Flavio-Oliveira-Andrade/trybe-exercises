e0e9dbee0829aaf55459186251d5f192b61c44e7
Flavio Andrade <foa12381@gmail.com>
2021-02-28 19:41:39 -0300

    exercises

--- Bloco-04_Introdução-a-javaScript-Dom-eventos-webSotage/Bloco-4.0-Introdução-Javascripts/exercicios3.js
+++ Bloco-04_Introdução-a-javaScript-Dom-eventos-webSotage/Bloco-4.0-Introdução-Javascripts/exercicios3.js
@@ -60,11 +60,9 @@ console.log("Valor de custo :" + ValorCusto);
 console.log("valor de lucro " + lucro);
 
 
-/*
-Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR.
-Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
-A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10.
-Para as faixas de impostos, use as seguintes referências:
+/* 11
+Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
 INSS
 Salário bruto até R$ 1.556,94: alíquota de 8%
 Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
@@ -82,7 +80,7 @@ Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: 
 Para pegar o valor do IR, temos um salário (já deduzido o INSS) entre R$ 1.903,99 e 2.826,65, sendo a alíquota, então, de 7.5%, com parcela de R$ 142,80 a deduzir do imposto. Assim, temos:
 R$ 2.670,00: salário com INSS já deduzido;
 7.5%: alíquota de imposto de renda;
-R$ 142,00 parcela a se deduzir do imposto.
+R$ 142,80 parcela a se deduzir do imposto.
 Fazendo a conta, temos: (7,5% de R$ 2.670,00) - R$ 142,80 = R$ 57,45
 O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
 Resultado: R$ 2.612,55.
@@ -90,4 +88,7 @@ Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?
 
 */
 
+let salario  ;
+let iss ;
+let ir ;
 
