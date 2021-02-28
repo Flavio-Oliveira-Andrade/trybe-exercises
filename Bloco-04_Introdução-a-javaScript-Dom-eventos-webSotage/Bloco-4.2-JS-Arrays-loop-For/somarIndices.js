10e257f533e0bc1d402efcfdfa53fe480d4e8285
Flavio Andrade <foa12381@gmail.com>
2021-02-28 19:43:08 -0300

    exercises

--- Bloco-04_Introdução-a-javaScript-Dom-eventos-webSotage/Bloco-4.2-JS-Arrays-loop-For/somarIndices.js
+++ Bloco-04_Introdução-a-javaScript-Dom-eventos-webSotage/Bloco-4.2-JS-Arrays-loop-For/somarIndices.js
@@ -13,3 +13,7 @@ for (i=0 ;i < numbers.length;i++){
 }
 console.log(`Valor total.. ${tota}`);
 console.log(`Valor da Media é.. ${tota/posical}`);
+
+
+// caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20".
+// Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
