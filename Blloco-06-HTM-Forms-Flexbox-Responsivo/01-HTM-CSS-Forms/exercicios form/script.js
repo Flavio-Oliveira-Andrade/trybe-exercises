22278af395ee22173883cb825b8ae4c25b2b3469
Flavio Andrade <foa12381@gmail.com>
2021-03-16 19:41:54 -0300

    teste54

--- Blloco-06-HTM-Forms-Flexbox-Responsivo/01-HTM-CSS-Forms/exercicios form/script.js
+++ Blloco-06-HTM-Forms-Flexbox-Responsivo/01-HTM-CSS-Forms/exercicios form/script.js
@@ -1,8 +1,5 @@
 let estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia','Ceará', 'DistritoFederal', 'EspíritoSanto', 'Goiás', 'Maranhão', 'MatoGrosso', 'MatoGrossoDoSul', 'MinasGerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'RioDeJaneiro', 'RioGrandeDoNorte', 'RioGrandeDoSul', 'Rondônia', 'Roraima', 'SantaCatarina', 'SaoPaulo', 'Sergipe', 'Tocantins'];
-
 let sigla = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
-
-
 
 function createElementOptionEstate(){
   let getElementOptions = document.querySelector('#station');
@@ -12,16 +9,9 @@ function createElementOptionEstate(){
         elementoption.innerHTML = elementEstado;
         elementoption.value = sigla[i]
           getElementOptions.appendChild(elementoption);
-
   }
-
 }
 createElementOptionEstate();
 
 
 
-// function createElementOptionEstate(){
-//   let getElementOptions = document.querySelector('#station');
-//   for (i = 0; i <= estados.length; i++) {
-//     $('select').append(`<option>${estados[i]}</option>`);
-//   }
