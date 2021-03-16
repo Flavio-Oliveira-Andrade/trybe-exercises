6025a773808d581b6c3d6171116985b08fd5c7ae
Flavio Andrade <foa12381@gmail.com>
2021-03-16 12:31:21 -0300

    estrutura do HTML Array dos estados

---
+++ Blloco-06-HTM-Forms-Flexbox-Responsivo/01-HTM-CSS-Forms/exercicios form/script.js
@@ -0,0 +1,26 @@
+let estados = [Acre, Alagoas, Amapá, Amazonas, Bahia,Ceará, DistritoFederal, EspíritoSanto, Goiás, Maranhão]
+let sigla = [AC, AL, AP, AM, BA, CE, DF, ES, GO, MA];
+
+
+Ceará (CE)
+Distrito Federal (DF)
+Espírito Santo (ES)
+Goiás (GO)
+Maranhão (MA)
+Mato Grosso (MT)
+Mato Grosso do Sul (MS)
+Minas Gerais (MG)
+Pará (PA)
+Paraíba (PB)
+Paraná (PR)
+Pernambuco (PE)
+Piauí (PI)
+Rio de Janeiro (RJ)
+Rio Grande do Norte (RN)
+Rio Grande do Sul (RS)
+Rondônia (RO)
+Roraima (RR)
+Santa Catarina (SC)
+São Paulo (SP)
+Sergipe (SE)
+Tocantins (TO)
