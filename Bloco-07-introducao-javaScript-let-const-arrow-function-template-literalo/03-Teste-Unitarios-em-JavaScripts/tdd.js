7ebfd252d2bd5595cc83e7f4c0b01db0395c832e
Flavio Andrade <foa12381@gmail.com>
2021-03-28 14:21:09 -0300

    exercises6

--- Bloco-07-introducao-javaScript-let-const-arrow-function-template-literalo/03-Teste-Unitarios-em-JavaScripts/tdd.js
+++ Bloco-07-introducao-javaScript-let-const-arrow-function-template-literalo/03-Teste-Unitarios-em-JavaScripts/tdd.js
@@ -2,11 +2,12 @@
 const assert = require('assert');
 
 const myFunction = (num1, num2) => {
-  if (typeof(num1 !== number || typeof(num2 !== number)) ){
+  if (typeof(num1) !== 'number' || typeof(num2) !== 'number'){
     throw new Error('Valores devem ser numeros')
   }
   return num1 / num2
 }
-// myFunction(6, 2)
 assert.strictEqual(typeof myFunction, 'function');
 assert.deepStrictEqual(myFunction(6, 2), 3, 'e maio que ');
+
+console.log(myFunction(6, '2'));
