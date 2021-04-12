b88b55f0edd321d22be662e73cda6ebc9f80cbdb
Flavio Andrade <foa12381@gmail.com>
2021-04-12 17:14:13 -0300

    exercises

---
+++ 09-bloco JavaScipts assincrono e Callbacks/javascripts Assincronas e Callbacks/testes/script1.js
@@ -0,0 +1,18 @@
+// const async
+// await
+
+function func1() {
+  setTimeOut(() => {
+    console.log('Função 1');
+  },3000)
+}
+func1()
+se
+function func2() {
+  setTimeOut(() => {
+    console.log('Função 2');
+  },3000)
+}
+func2()
+
+
