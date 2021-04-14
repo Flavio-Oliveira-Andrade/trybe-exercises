5a3f68a9619c9ca5ae87de10a9aaa50ab1e7b3c6
Flavio Andrade <foa12381@gmail.com>
2021-04-14 10:43:17 -0300

    exercises

---
+++ 09-bloco JavaScipts assincrono e Callbacks/javascripts Assincronas e Callbacks/testes/fetch.js
@@ -0,0 +1,4 @@
+//fetch() , chamadas de url, trata de uma função assincrona, baseada em uma promise
+//a função fech() recebe dois paramentros ,  1 url, 2 um objeto contendo algumas informações
+// esse objeto contem chave valor , precissamos em resposta json() que retorna uma Promises
+// encadiamos um .then method ,
