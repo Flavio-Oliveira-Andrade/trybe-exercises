995b47bf4953b27085d8506ea1027ee0b53ecbb5
Flavio Andrade <foa12381@gmail.com>
2021-04-27 11:11:40 -0300

    react

--- 11 - bloco  - Introdução ao React/1 - introdução - front-end/testando-meu-computador/src/App.js
+++ 11 - bloco  - Introdução ao React/1 - introdução - front-end/testando-meu-computador/src/App.js
@@ -1,11 +1,27 @@
 import  React from 'react';
+import Header from './componentes/header'
+import Body from './componentes/body'
+import Footer from './componentes/footer'
+import Props from './componentes/props'
 
 // exemplo export default function app() {}
 function App() {
+  const html = '511111 trybe meses de curso'
+  const css = '1 mes de curso'
+  const script = ' meses'
+  const ls = ( ) => '10 projetos'
+
   return (
     <>
-      <h1>Estudando React na Trybe</h1>
-      <p>Aluno: <strong>Flavio Oliveira</strong></p>
+      <Header/>
+      <Body/>
+      <Footer/>
+      <Props
+        HTML= {html}
+        CSS= {css}
+        Javascript= {script}
+        React= {ls()}
+        />
     </>
   )
 }
---
+++ 11 - bloco  - Introdução ao React/1 - introdução - front-end/testando-meu-computador/src/componentes/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from '../logo.svg'
+
+function footer() {
+  return (
+    <img src={Logo}/>
+  )
+}
+
+export default footer;
