bc81ea29fb4ab6f75ca6f6d261b63bb81a462f94
Flavio Andrade <foa12381@gmail.com>
2021-05-01 10:49:35 -0300

    css

---
+++ 11 - bloco  - Introdução ao React/2 - introdução - React/myapp/dia2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Buton from './componentes/class'
+
+
+function App() {
+  return (
+    <>
+      <Buton />
+
+    </>
+  )
+}
+
+export default App;
---
+++ 11 - bloco  - Introdução ao React/2 - introdução - React/myapp/dia2/src/componentes/class.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Button() {
+  return (
+    <div>
+      <h1> Flavio Oliveira </h1>
+      <button>Click me</button>
+    </div>
+  );
+}
