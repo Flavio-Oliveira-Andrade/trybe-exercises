ad392dff1bf9ebcb201095c3a109c6e454dfcde2
Flavio Andrade <foa12381@gmail.com>
2021-05-06 10:07:31 -0300

    evente em react

--- 12 - bolco componentes com Estado, eventos e formulario/1 - Componentes com estado e evento/my-app-state/src/App.js
+++ 12 - bolco componentes com Estado, eventos e formulario/1 - Componentes com estado e evento/my-app-state/src/App.js
@@ -3,28 +3,26 @@ não é a maneira correta de se criar eventos
 em React! A função se refere ao componente,
 então deve ser parte de sua classe! */
 import React from 'react';
-
-function handleClick() {
-  console.log('clicou')
-}
-function handleClick1() {
-  console.log('clicou1')
-}
-function handleClick2() {
-  console.log('clicou2')
-}
-
 class App extends React.Component{
    /* Repare que, diferentemente do HTML, no
   JSX você associa uma função a um evento
   passando a própria função entre chaves `{}` */
+  handleClick() {
+    console.log('clicou')
+  }
+  handleClick1() {
+    console.log('clicou1')
+  }
+  handleClick2() {
+    console.log('clicou2')
+  }
   render() {
 
     return (
       <div>
-        <button onClick={handleClick}> carrinho </button>
-        <button onClick={handleClick1}> carrinho1 </button>
-        <button onClick={handleClick2}> carrinho2 </button>
+        <button onClick={this.handleClick}> carrinho </button>
+        <button onClick={this.handleClick1}> carrinho1 </button>
+        <button onClick={this.handleClick2}> carrinho2 </button>
      </div>
     )
   }
