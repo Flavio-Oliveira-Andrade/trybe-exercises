1c179a1640b04d157a433385c5865025960ae575
Flavio Andrade <foa12381@gmail.com>
2021-05-07 09:48:53 -0300

    evente em react

--- 12 - bolco componentes com Estado, eventos e formulario/2 - Formulario no React/form/form/src/components/form.jsx
+++ 12 - bolco componentes com Estado, eventos e formulario/2 - Formulario no React/form/form/src/components/form.jsx
@@ -1,20 +1,73 @@
 import React from 'react';
 
 class Form extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
+  constructor() {
+    super()
 
+    this.handleChange = this.handleChange.bind(this)
+
+    this.state = {
+      estadoFavorito:'',
+      email: '',
+      idade:0,
+      vaiComparecer: false,
+      palavraChaveFavorita: '',
 
     }
 
   }
+
+  handleChange({target}) {
+    const {name, } = target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+
+    this.setState({
+      [name]: value
+    })
+  }
+
   render(){
     return (
       <div>
         <span>Formulario em React</span>
         <form>
+          <label>
+            Diga qual éo seu Estado favorito do Brasil ou do React , vocẽ quem sabe
+            <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+          </label>
 
+          <label>
+            Email
+            <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
+          </label>
+
+          <label>
+            Nome
+            <input type="nome" name="nome" value={this.state.nome} onChange={this.handleChange} />
+          </label>
+
+          <label>
+            idade
+            <input type="number" name="idade" value={this.state.idade} onChange={this.handleChange}/>
+          </label>
+
+          <label>
+            <input type="checkbox" name="vaiComparecer" value={this.state.vaiComparecer} onChange={this.handleChange} />
+          </label>
+
+          <label>
+            escolha sua palavra chave favorita
+            <select
+            name="palavrachaveFavorita"
+            value={this.state.palavraChaveFavorita}
+            onChange={this.handleChange}
+            >
+              <option value="Componente">Componentes </option>
+              <option value="Estado">Estado </option>
+              <option value="Evento">Evento </option>
+              <option value="Props">Props </option>
+            </select >
+          </label>
         </form>
       </div>
     )
