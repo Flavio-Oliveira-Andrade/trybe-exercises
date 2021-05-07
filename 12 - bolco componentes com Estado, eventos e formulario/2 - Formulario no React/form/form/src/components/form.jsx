da5f814e766b7410f60eb990b3415704f96b0a17
Flavio Andrade <foa12381@gmail.com>
2021-05-07 07:31:47 -0300

    evente em react

---
+++ 12 - bolco componentes com Estado, eventos e formulario/2 - Formulario no React/form/form/src/components/form.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+
+    }
+
+  }
+  render(){
+    return (
+      <div>
+        <span>Formulario em React</span>
+        <form>
+
+        </form>
+      </div>
+    )
+  }
+
+
+}
+export default Form;
