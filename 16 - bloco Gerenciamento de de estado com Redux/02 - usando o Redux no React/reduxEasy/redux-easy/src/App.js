1d2109a8ad4b49bbf7f4b2e04c57a87ffa4c88f1
Flavio Andrade <foa12381@gmail.com>
2021-06-07 15:34:41 -0300

    add form

--- 16 - bloco Gerenciamento de de estado com Redux/02 - usando o Redux no React/reduxEasy/redux-easy/src/App.js
+++ 16 - bloco Gerenciamento de de estado com Redux/02 - usando o Redux no React/reduxEasy/redux-easy/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Password" />
+        <input type="text" placeholder="Filme favorito" />
+        <button type="submit">Enviar</button>
 
+      </form>
     </div>
   );
 }
