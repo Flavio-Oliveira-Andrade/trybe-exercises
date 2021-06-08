2c301405db8c24d17c318037952efba485401a12
Flavio Andrade <foa12381@gmail.com>
2021-06-08 09:53:30 -0300

    state

--- 16 - bloco Gerenciamento de de estado com Redux/02 - usando o Redux no React/reduxEasy/redux-easy/src/App.js
+++ 16 - bloco Gerenciamento de de estado com Redux/02 - usando o Redux no React/reduxEasy/redux-easy/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <form>
-        <input type="text" placeholder="Nome" />
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Password" />
-        <input type="text" placeholder="Filme favorito" />
-        <button type="submit">Enviar</button>
-
-      </form>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: [],
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input type="text" placeholder="Nome" />
+          <input type="email" placeholder="E-mail" />
+          <input type="password" placeholder="Password" />
+          <input type="text" placeholder="Filme favorito" />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
