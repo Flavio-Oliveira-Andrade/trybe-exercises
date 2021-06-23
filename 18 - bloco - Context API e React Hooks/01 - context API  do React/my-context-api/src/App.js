d2e1e157a00758a105c0258f97692c1a8060b6ca
Flavio Andrade <foa12381@gmail.com>
2021-06-23 09:39:20 -0300

    useState3

--- 18 - bloco - Context API e React Hooks/01 - context API  do React/my-context-api/src/App.js
+++ 18 - bloco - Context API e React Hooks/01 - context API  do React/my-context-api/src/App.js
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [repositories, setRepositories] = useState([
-    { id: 1, name: 'repo-1'},
-    { id: 1, name: 'repo-2'},
-    { id: 1, name: 'repo-3'},
-  ]);
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function getRepositories(){
+      const response =await fetch('https://api.github.com/users/Flavio-Oliveira-Andrade/repos')
+      const data = await response.json();
+      setRepositories(data)
+    }
+    getRepositories()
+  },[])
+  // function handleAddRepository(){
+  //   setRepositories([
+  //     ...repositories,
+  //     {id: Math.random(), name: 'repo-match' }
+  //   ])
+  // }
+  function handleFavorites(id) {
+    const newRepositories = repositories.map(rep => {
+      return rep.id === id ?{...rep , favorite: !rep.favorite} : rep
+    })
+
+    setRepositories(newRepositories)
+  }
 
   return (
     <div >
-      hello flavio
+      <ul>
+        {repositories.map((rep) =>
+        <li key={rep.id}>
+          {rep.name}
+          {rep.favorite && <span>(favorito)</span>}
+         <button onClick={()=>handleFavorites(rep.id)}>
+           { !rep.favorite ? 'Favoritar' : 'Desfavoritar' }
+           </button></li>
+        )}
+      </ul>
+      {/* <button type="button" onClick={handleAddRepository}>Add</button> */}
+
     </div>
   );
 }
