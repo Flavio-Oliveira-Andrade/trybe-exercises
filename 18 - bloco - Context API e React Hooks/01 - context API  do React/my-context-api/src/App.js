05971abb8c53ce66527ef8746ea36809d1266b89
Flavio Andrade <foa12381@gmail.com>
2021-06-23 07:40:43 -0300

    useState2

--- 18 - bloco - Context API e React Hooks/01 - context API  do React/my-context-api/src/App.js
+++ 18 - bloco - Context API e React Hooks/01 - context API  do React/my-context-api/src/App.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 
 function App() {
-  const [] = useState();
+  const [repositories, setRepositories] = useState([
+    { id: 1, name: 'repo-1'},
+    { id: 1, name: 'repo-2'},
+    { id: 1, name: 'repo-3'},
+  ]);
 
   return (
     <div >
