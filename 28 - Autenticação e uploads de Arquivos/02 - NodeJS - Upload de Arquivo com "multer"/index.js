9a10ac57613a25ee29aa3b6df5ef5a572d9a4d81
Flavio Andrade <foa12381@gmail.com>
2021-09-20 09:24:01 -0300

    estruct directory

--- 28 - Autenticação e uploads de Arquivos/02 - NodeJS - Upload de Arquivo com "multer"/index.js
+++ 28 - Autenticação e uploads de Arquivos/02 - NodeJS - Upload de Arquivo com "multer"/index.js
@@ -1,1 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
 require('dotenv').config();
+
+const PORT_URL = '3333'
+
+app.listen(()=>console.log(`Rodando na port${PORT_URL}`))
+
