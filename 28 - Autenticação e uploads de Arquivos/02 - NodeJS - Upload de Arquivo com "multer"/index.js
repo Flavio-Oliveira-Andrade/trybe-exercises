3570e386696a2a7bcc3f7ef108fbefadf7c27803
Flavio Andrade <foa12381@gmail.com>
2021-09-20 12:15:38 -0300

    multer

--- 28 - Autenticação e uploads de Arquivos/02 - NodeJS - Upload de Arquivo com "multer"/index.js
+++ 28 - Autenticação e uploads de Arquivos/02 - NodeJS - Upload de Arquivo com "multer"/index.js
@@ -1,12 +1,37 @@
 const express = require('express')
+const multer = require('multer')
 
 const app = express()
 
 app.use(express.json())
 
-require('dotenv').config();
 
-const PORT_URL = '3333'
+const PORT_URL = '3900'
 
-app.listen(()=>console.log(`Rodando na port${PORT_URL}`))
+
+const diskStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "uploads/")
+  },
+  filename:(req, file, callback) => {
+    callback(null, Date.now() + '-' + file.originalname)
+  },
+})
+
+const uploads = multer({ diskStorage })
+
+// const uploads = multer({
+//   dest: 'uploads/'
+// })
+
+app.get('/ping', (req, res) => {
+  res.send('pong')
+ })
+
+app.post('/:uplod',uploads.single("arquivo"), (req, res)=>{
+ res.send('uplods efetuado com sucesso')
+})
+
+
+app.listen(PORT_URL,()=>console.log(`Rodando na port${PORT_URL}`))
 
