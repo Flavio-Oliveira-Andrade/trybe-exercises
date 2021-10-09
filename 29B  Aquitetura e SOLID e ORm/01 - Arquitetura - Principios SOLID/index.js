a2e06f33e051070fa7466f79358890754ced2621
Flavio Andrade <foa12381@gmail.com>
2021-10-09 09:13:10 -0300

    adiciona test

--- 29B  Aquitetura e SOLID e ORm/01 - Arquitetura - Principios SOLID/index.js
+++ 29B  Aquitetura e SOLID e ORm/01 - Arquitetura - Principios SOLID/index.js
@@ -49,3 +49,106 @@ const students = [
 ];
 
 setApproved(students);
+
+// ./index.js
+
+/* "Converter" */
+const percentageGradesIntoLetters = ({ name, disciplines }) => ({
+  name,
+  disciplines: disciplines.map(({ name, grade }) => {
+    let letterGrade;
+
+    if (grade >= 0.9) letterGrade = 'A';
+    else if (grade >= 0.8) letterGrade = 'B';
+    else if (grade >= 0.7) letterGrade = 'C';
+    else if (grade >= 0.6) letterGrade = 'D';
+    else if (grade >= 0.1) letterGrade = 'E';
+    else letterGrade = 'F';
+
+    return { name, grade, letterGrade };
+  })});
+
+/* "Determinar" */
+const approvedStudents = ({ disciplines }) =>
+  disciplines.every(({ grade }) => grade > 0.7);
+
+/* "Atualizar" */
+const updateApprovalData = ({ name: studentName, disciplines }) => {
+  console.log(`A pessoa com nome ${studentName} foi aprovada!`);
+
+  disciplines.map(({ name, letterGrade }) =>
+    console.log(`${name}: ${letterGrade}`));
+};
+
+function setApproved(students) {
+  students
+    .map(percentageGradesIntoLetters)
+    .filter(approvedStudents)
+    .map(updateApprovalData);
+}
+
+/* Exemplo de execução */
+// ...
+
+/*
+  Não se esqueça que é necessário exportar ao final as
+  funções para que você possa testa-las de forma unitária
+*/
+module.exports = {
+  percentageGradesIntoLetters,
+  approvedStudents,
+  updateApprovalData,
+  setApproved,
+};
+
+
+/* Apoio para a função `getGradeLetter`, lembraremos disso mais a frente */
+const GRADE_DICT = {
+  9: 'A',
+  8: 'B',
+  7: 'C',
+  6: 'D',
+  1: 'E',
+  };
+
+  const gradeKeys = Object.keys(GRADE_DICT);
+
+  /* Função menor para remover o uso excessivo de `if{}else`s */
+  const getGradeLetter = (gradeNumber) => {
+    let letterGrade = 'F';
+
+    for (let i = 0; i < gradeKeys.length; i += 1) {
+      if (gradeNumber >= gradeKeys[i]) {
+        letterGrade = GRADE_DICT[gradeKeys[i]];
+        break;
+      }
+    }
+
+    return letterGrade;
+  };
+
+  /* Coletar notas */
+  const getLetterGrades = ({ name, grade }) => ({
+    name,
+    grade,
+    letterGrade: getGradeLetter(grade)});
+
+  /* "Converter" */
+  const percentageGradesIntoLetters = ({ name, disciplines, school }) => ({
+    name,
+    school,
+    disciplines: disciplines.map(getLetterGrades)});
+
+  /* Exemplo de execução */
+  // ...
+
+  /*
+    Vamos exportar também nossa nova função de `Coletar notas` para testes
+  */
+  // module.exports = {
+  //   percentageGradesIntoLetters,
+  //   approvedStudents,
+  //   updateApprovalData,
+  //   setApproved,
+    getLetterGrades,
+  // };
