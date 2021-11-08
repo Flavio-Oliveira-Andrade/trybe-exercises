ae9b5207ee05d6e73579808186b52996ebf64e02
Flavio Andrade <foa12381@gmail.com>
2021-11-08 10:03:53 -0300

    task ccreate front-end

--- 30 -  Sockets/03 - Sockets - Sockets.io/index.js
+++ 30 -  Sockets/03 - Sockets - Sockets.io/index.js
@@ -1,10 +1,25 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
+ const express = require('express');
+ const app = express();
+ const http = require('http').createServer(app);
 
-app.get('/', (req, res)=>{
-  res.sendFile(__dirname + '/index.html');
-})
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  }});
 
-const PORT ='3000'
-app.listen(PORT, ()=> console.log('listening on port',PORT))
+io.on('connection', (socket) => {
+  socket.name = 'aceitos';
+  console.log(`Usuário conectado. ID: ${socket.id} ${socket.name} `);
+  socket.on('ping',()=>{
+    console.log(`Usuário conectado`)
+  })
+});
+
+ app.get('/', (req, res) => {
+   res.sendFile(__dirname + '/index.html');
+ });
+
+ http.listen(3000, () => {
+   console.log('Servidor ouvindo na porta 3000');
+ });
