47b115960ebac1f2a72ddefdee518bc16edd4c68
Flavio Andrade <foa12381@gmail.com>
2021-12-15 08:38:37 -0300

    for and filter em python

---
+++ 32 - Introdução a Python/02 - Apredendo Python/index.py
@@ -0,0 +1,8 @@
+fruits = ["maça", "pera", "laranja", "ovo"]
+for fruit in fruits:
+    if len(fruit) > 3:
+        print(len(fruit))
+        print(fruit[3] )
+    else:
+        print(len(fruit))
+        print(fruit[2] )
